4290b7afabea9ca505069aea6bf3bc61ac51e190
Temirbek <temirbek@gmail.com>
2026-01-13 12:33:22 +0600

    misc

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors')
 module.exports = {
     content: [
         './templates/**/*.html',
-        './templates/*.html',
-        './static/**/*.js',
+        './**/templates/**/*.html',
     ],
     theme: {
         extend: {},
-        colors: {
-            transparent: 'transparent',
-            current: 'currentColor',
-            black: colors.black,
-            white: colors.white,
-            emerald: colors.emerald,
-            indigo: colors.indigo,
-            yellow: colors.yellow,
-            stone: colors.stone,
-            sky: colors.sky,
-            neutral: colors.neutral,
-            gray: colors.gray,
-            slate: colors.slate,
-        }
     },
     plugins: [],
 }
